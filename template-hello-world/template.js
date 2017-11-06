c8a827262d3010f53d14bb8633bf93ae6052e4f1
Yordan Pavlov <ThuF@users.noreply.github.com>
2017-11-06 13:51:07 +0200

    Update template.js

--- template-hello-world/template.js
+++ template-hello-world/template.js
@@ -1,12 +1,12 @@
 exports.getTemplate = function() {
 	return {
-			"name": "Hello World",
-			"description": "Hello World Template",
-			"sources": [{
-				"location": "template-hello-world/service.js", 
-				"action": "generate",
-				"rename": "{{fileName}}.js"
-		    }],
-		    "parameters": []
+		"name": "Hello World",
+		"description": "Hello World Template",
+		"sources": [{
+			"location": "/template-hello-world/service.js", 
+			"action": "generate",
+			"rename": "{{fileName}}.js"
+		}],
+		"parameters": []
 	};
 };
